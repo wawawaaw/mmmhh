a95bf15ebd7ed259b8dc359a39766099435a7c8a
wawawawawa <yannickwang933002@gmail.com>
2019-05-19 01:16:18 +0200

    update

--- bot.py
+++ bot.py
@@ -144,24 +144,18 @@ async def roulette(ctx):
     
 @bot.command()
 async def connect(ctx):
-	if not discord.opus.is_loaded():
-	# the 'opus' library here is opus.dll on windows
-	# or libopus.so on linux in the current directory
-	# you should replace this with the location the
-	# opus library is located in and with the proper filename.
-	# note that on windows this DLL is automatically provided for you
-		discord.opus.load_opus('opus.dll')
-	
     if ctx.message.author.guild_permissions.administrator:
-        channel = ctx.message.author.voice.channel
-        if not channel:
-            await ctx.send("You are not connected to a voice channel")
-            return
-        voice = get(bot.voice_clients)
-        if voice and voice.is_connected():
-            await voice.move_to(channel)
-        else:
-            voice = await channel.connect()
+        if not discord.opus.is_loaded():
+            discord.opus.load_opus('opus')
+            channel = ctx.message.author.voice.channel
+            if not channel:
+                await ctx.send("You are not connected to a voice channel")
+                return
+            voice = get(bot.voice_clients)
+            if voice and voice.is_connected():
+                await voice.move_to(channel)
+            else:
+                voice = await channel.connect()
     else:
         await ctx.send('fdp ta pas la perm')
         
